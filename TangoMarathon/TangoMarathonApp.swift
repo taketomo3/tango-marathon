21e352548e99028df39963ff5feeef1053239245
竹田智哉 <andalso316@gmail.com>
2022-09-13 19:43:00 +0900

    init

---
+++ TangoMarathon/TangoMarathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangoMarathonApp.swift
+//  TangoMarathon
+//
+//  Created by 竹田智哉 on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct TangoMarathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TangoMarathon/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TangoMarathon
+//
+//  Created by 竹田智哉 on 2022/09/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
